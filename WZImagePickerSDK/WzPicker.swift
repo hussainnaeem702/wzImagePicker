f565dccb299e97acbf768bf443ecdd0fc13f2434
Naeem Hussain <naeem.hussain@whizpool.com>
2020-02-19 15:08:35 +0500

    xax

--- WZImagePickerSDK/WzPicker.swift
+++ WZImagePickerSDK/WzPicker.swift
@@ -21,6 +21,11 @@ public class HelloWorld {
     public func showPickerController(_ fromViewController : UIViewController)
     {
         let assetBundle = Bundle(for: type(of: self))
+        let bundlePath = assetBundle.path(forResource: "WZImagePickerSDK", ofType: "bundle")
+        if bundlePath != nil {
+            assetBundle = Bundle(path: bundlePath ?? "")
+        }
+        //let assetBundle = Bundle(for: type(of: self))
         let wzPicker = WZAlbumsViewController(nibName: "WZAlbumsViewController", bundle: assetBundle)
         fromViewController.present(wzPicker, animated: true, completion: nil)
     }
